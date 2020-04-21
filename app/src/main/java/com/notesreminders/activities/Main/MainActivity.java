285e1ac0513b10bff4ca5610952c7a3fc2531dae
RouSage <therousage@gmail.com>
2020-04-21 18:28:34 +0300

    Initial commit

---
+++ app/src/main/java/com/notesreminders/activities/Main/MainActivity.java
@@ -0,0 +1,16 @@
+package com.notesreminders.activities.Main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.notesreminders.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
